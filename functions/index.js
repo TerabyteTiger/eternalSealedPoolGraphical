1c7703c77eab2940ac0b206fd1055ffd2427aeef
Tyler <12vanblart@gmail.com>
2019-02-20 18:37:28 +0000

    User Init function v1

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,83 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+require("firebase/firestore");
+var db = firebase.firestore();
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.initUser = functions.auth.user().onCreate(user => {
+	// ...
+	const email = user.email;
+	db.collection("users")
+		.add({
+			user: email,
+			mono: {
+				playables: {
+					fire: 0,
+					time: 0,
+					justice: 0,
+					primal: 0,
+					shadow: 0,
+				},
+				removal: {
+					fire: 0,
+					time: 0,
+					justice: 0,
+					primal: 0,
+					shadow: 0,
+				},
+			},
+			dual: {
+				playables: {
+					praxis: 0,
+					rakano: 0,
+					combrei: 0,
+					elysian: 0,
+					hooru: 0,
+					argenprot: 0,
+					skycrag: 0,
+					feln: 0,
+					stonescar: 0,
+					xenan: 0,
+				},
+				removal: {
+					praxis: 0,
+					rakano: 0,
+					combrei: 0,
+					elysian: 0,
+					hooru: 0,
+					argenprot: 0,
+					skycrag: 0,
+					feln: 0,
+					stonescar: 0,
+					xenan: 0,
+				},
+				fixing: {
+					praxis: 0,
+					rakano: 0,
+					combrei: 0,
+					elysian: 0,
+					hooru: 0,
+					argenprot: 0,
+					skycrag: 0,
+					feln: 0,
+					stonescar: 0,
+					xenan: 0,
+				},
+			},
+			tri: {
+				playables: {
+					jennev: 0,
+					ixtun: 0,
+					winchest: 0,
+					kerendon: 0,
+					auralian: 0,
+				},
+			},
+		})
+		.then(function(docRef) {
+			console.log("Document written with ID: ", docRef.id);
+		})
+		.catch(function(error) {
+			console.error("Error adding document: ", error);
+		});
+});
